6cea404f23d9c19903ac491299118f309696c5dc
Tarik Desire <tprdesire@gmail.com>
2018-10-08 14:48:00 -0400

    Added palindrome function

--- drills.js
+++ drills.js
@@ -18,9 +18,42 @@ function main() {
   lor.set('Ent', 'Treebeard');
   console.log(lor.get('Maiar'));
 
-  console.log(lor._slots);
+  console.log(lor);
 
+  console.log(permIsPalindrome('acecarr'));
+  console.log(permIsPalindrome('north'));  
+  console.log(permIsPalindrome('mmada'));
+  console.log(permIsPalindrome('banana'));
 }
+
+// input: acecarr
+// output: true
+
+// is the string odd or even?
+
+function permIsPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  let permMap = new Map();
+  for (let i = 0; i < str.length; i++) {
+    if(permMap.has(str[i])) {
+      permMap.delete(str[i]);
+    }
+    else if (!permMap.has(str[i])) {
+      permMap.set(str[i], i);
+    }
+  }
+
+  if (str.length % 2 === 0 && permMap.size === 0) {
+    return true;
+  }
+
+  if (str.length % 2 === 1 && permMap.size === 1) {
+    return true;
+  }
+
+  return false;
+}
+// function has an O(n) runtime
 
 main();
 
