9c073345e2d84f3d3e130f3ad3408861a43d84d7
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-08 09:27:17 -0700

    class created, get and set done

---
+++ drills.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const hashMapClass = require ('./buildHashMap');
+
+function main() {
+  let lor = new hashMapClass();
+
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+  console.log(lor.get('Maiar'));
+
+  console.log(lor._slots);
+
+}
+
+main();
+
